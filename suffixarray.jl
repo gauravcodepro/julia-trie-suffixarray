e65ca2c437e6cd7dd5544c570c7d1503455b0887
Gaurav <gaurav.sablok@uni-potsdam.de>
2024-07-08 13:11:35 +0200

    2024-7-8 updated
    generating the suffix arrays for all the reads and then implementing a trie in RUST to make a node graph. Generate the hashes in less than hour for 40X coverage. Next going to code the remaining part with the suffix as $ in RUST so that it can compile the build.

---
+++ suffixarray.jl
@@ -0,0 +1,52 @@
+#! /usr/bin/env julia
+# Author Gaurav
+# Universitat Potsdam
+# Date 2024-7-8
+
+# implementation of suffix arrays for the PacbioHifi reads and sorting them in trie graphs.
+# will generate the suffix arrays for all of them and you can fetch the suffix arrays.
+# implementing a rust crate to implement the trie graphs which will take this as input.
+# sort the pangenomes mapping reads as suffix arrays.
+
+function generatesuffixarray(readsfiles)
+    if !readfiles
+        error("readfiles are needed for the suffixarray")
+    end
+    return
+    header = Any[]
+    sequence = Any[]
+    if readsfiles
+        readfiles = readlines(open(readfiles))
+        for i in 1:length(readfiles)
+            if startswith(readfiles[i], "@")
+                push(header,split(readfiles[i], " ")[1])
+                push(sequence, chomp(readfiles[i+1]))
+            end
+        end
+        suffixarraystore = Dict(String,Union{Int32, String})()
+         # indexing the first slot of the variable for the suffix array rather than making slices
+         # instead of sorting a later has, imimplemented a position hash so that where it is generating
+         # it is storing the indices hash.
+         # in the another implementation i am puting the prefix and the suffix merge using $
+        for i in 1:length(sequence)-1
+            if i == 1
+                suffixarraystore[header] = [i, string(sequence[1])]
+            end
+            if i != 1
+                suffixarraystore[header] = [i, sequence[i=length(sequence)]]
+            end
+        end
+        lastbitlengthstore = Dict(Int,Union{Int32,String)()
+        for i in 1:length(sequence)
+            lastbitlengthstore["header"] = [length(seq), seq[length(sequence)]]
+        end
+    end
+end
+
+
+
+
+
+
+
+
